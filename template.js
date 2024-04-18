931c7917a454bf54b0d33c3894d2596b1a6c2cf1
atakanbaskor <atakanbaskor@hotmail.com>
2024-04-18 14:32:01 +0300

    variables

--- template.js
+++ template.js
@@ -13,3 +13,23 @@ console.info("console.info");
 console.error("console.error");
 // alert("Alert");
 // window.alert("Alert");
+
+//var : global değişken
+//let : local değişken
+//const : local değişken Değer değiştirilemez. (Constant)
+
+//Variable
+// var,let, const
+var Name = "Atakan";
+console.log("İsim: " + Name);
+
+let CompanyName = "Berga";
+console.warn(CompanyName);
+
+let CompanyName2 = "Berga";
+CompanyName2 = "Berga Danismanlik";
+console.log(CompanyName2);
+
+const CompanyName3 = "Anomim";
+// CompanyName3 = "Limited";
+console.log(CompanyName3);
